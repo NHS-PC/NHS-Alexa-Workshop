f6720bff5671522c54a4e03d74403ffa44eac67b
Davis <dbkeene.tsyc@gmail.com>
2017-11-19 21:21:06 -0500

    Touched up code

--- SnowDay/mainSkill.py
+++ SnowDay/mainSkill.py
@@ -13,16 +13,16 @@ from data.weatherData import X_Data, Y_Data
 app = Flask(__name__)
 ask = Ask(app, "/")
 
-
 @ask.launch
 def greeting():
     msg = "Welcome to the Snow Day Calculator, powered by the Dark Sky API. What is your zip code?"
     return question(msg)
 
-
 @ask.intent("PredictIntent", convert={'prediction': str})
+
 # Prompt the user to enter their ZIP
 def predict(prediction):
+
     zipurl = "http://api.zipasaur.us/zip/" + str(prediction)
     zippage = urllib.urlopen(zipurl)
     cityresult = json.loads(zippage.read())
@@ -55,7 +55,7 @@ def predict(prediction):
 
     # fitting the data to the tree
 
-    y = np.array(Y_Data).reshape(line, )
+    y = np.array(Y_Data).reshape(line,)
 
     clf = svm.SVC(probability=True, kernel='linear', tol=0.001, C=0.95, class_weight='balanced')
 
@@ -63,9 +63,9 @@ def predict(prediction):
 
     coordinates = prediction
 
-    # Get the Latitude and Longitude based off of the zipcode, to be used for user input
+    #Get the Latitude and Longitude based off of the zipcode, to be used for user input
 
-    url = "https://api.darksky.net/forecast/" + API_KEY + "/" + lat + "," + str(long)
+    url = "https://api.darksky.net/forecast/"+API_KEY+"/"+lat+","+str(long)
 
     # API Request
     page = urllib.urlopen(url)
@@ -74,21 +74,21 @@ def predict(prediction):
 
     # Get city name
 
-    cityurl = "http://api.zipasaur.us/zip/" + str(coordinates)
+    cityurl = "http://api.zipasaur.us/zip/"+str(coordinates)
     citypage = urllib.urlopen(cityurl)
     cityresult = json.loads(citypage.read())
     city = cityresult['city']
     state = cityresult['state_full']
 
-    # Functions to get features from the API call
+# Functions to get features from the API call
 
-    # Get section of America, southern states are more prone to snow days!
+# Get section of America, southern states are more prone to snow days!
     latCar = round(float(lat))
     cat = 0
 
-    if latCar < 35:
+    if latCar<35:
         cat = 2
-    elif latCar >= 35 and latCar <= 40:
+    elif latCar>=35 and latCar<=40:
         cat = 1
     else:
         cat = 0
@@ -99,19 +99,19 @@ def predict(prediction):
     # Get the chance of precipitation NEED TO CHANGE THIS
     total = 0
     count = 0
-    for i in range(0, 12):
+    for i in range(0,12):
         n = result['hourly']['data'][i]['precipProbability']
-        total += n
-        count += 1
-    prob = round(total / count, 2)
+        total+=n
+        count+=1
+    prob = round(total/count,2)
 
     # Average Temp over 12 hours, return 1 if temp average temp is under 32 degrees
     temperature = result['daily']['data'][0]['temperatureMin']
     tempc = result['hourly']['data'][0]['temperature']
     temp = 0
-    if (temperature > 32):
+    if(temperature>32):
         temp = 0
-    elif temperature <= 32:
+    elif temperature<=32:
         temp = 1
 
     # Get current wind speed, return 0 if not blizzard and 1 if blizzard
@@ -119,10 +119,10 @@ def predict(prediction):
 
     # Get the type of precipitation (if snow, return 1. if not, return 0.25. This is the attenuation factor.)
     type = 0
-    for i in range(1, 6):
+    for i in range(1,6):
         try:
             pretype = result['hourly']['data'][i]['precipType']
-            if (pretype == "snow"):
+            if(pretype == "snow"):
                 type = 1
             elif pretype == "rain":
                 type = 2
@@ -133,12 +133,14 @@ def predict(prediction):
         except:
             pass
 
+
     # Get the estimated accumulation of snow (BIGGEST FACTOR)
     accumulation = 0
-    try:
-        accumulation = result['daily']['data'][0]['precipAccumulation']
-    except:
-        pass
+    for i in range(0,6):
+        try:
+            accumulation += result['daily']['data'][i]['precipAccumulation']
+        except:
+            pass
 
     # Get a weather summary from Dark Sky
     summary = (result['currently']['summary'])
@@ -152,53 +154,37 @@ def predict(prediction):
 
     # DATA: TYPE, ACCUMULATION, CHANCE, TEMP, SPEED, CATEGORY, STORM_DISTANCE (organized in order of importance)
 
-    features = [type, accumulation, prob, temp, round(speed, 0), cat]
+    features = [type,accumulation,prob,temp, round(speed,0), cat]
 
     # Prediction index
     arr = np.asarray(features).ravel()
 
     array = [arr]
 
-    #   prediction1 = clf.predict(array)
+#   prediction1 = clf.predict(array)
     ans = clf.predict_proba(array)
     finalPrediction = (ans.item((0, 1)) * 100)
 
-    print coordinates
-    print finalPrediction
-
-    #    # Override the print function IF something occurs
+#    # Override the print function IF something occurs
     try:
         if finalPrediction < 25:
-            if type != 1:
-                return question(
-                    "There is a small chance of a snow day in {}, {}, because it isn't supposed to snow today. The forecast calls for {}. Would you like to ask again?".format(
-                        city, state, summary))
+            if type !=1:
+                return question("There is a small chance of a snow day in {}, {}, because it isn't supposed to snow today. The forecast calls for {}. Would you like to ask again?".format(city,state,summary))
 
             if temp == 0:
-                return question(
-                    "There is a small chance of a snow day in {}, {}, because it isn't cold enough. Right now, it is about {} degrees outside, with a low of {} degrees for the day. Would you like to ask again?".format(
-                        city, state, tempc, temperature))
+                return question("There is a small chance of a snow day in {}, {}, because it isn't cold enough. Right now, it is about {} degrees outside, with a low of {} degrees for the day. Would you like to ask again?".format(city,state,tempc,temperature))
 
-            if accumulation <= 1 and cat <= 1:
-                return question(
-                    "There is a small chance of a snow day in {}, {}, because the forecast calls for {}, and less than an inch of snowfall at most. Would you like to ask again?".format(
-                        city, state, summary))
+            if accumulation <= 2 and cat<=1:
+                return question("There is a small chance of a snow day in {}, {}, because the forecast calls for {}, and less than an inch of snowfall in the next 6 hours. Would you like to ask again?".format(city,state,summary))
 
         message = summary.encode('utf-8')
 
         if finalPrediction > 75:
-            return question(
-                "There is a {} percent chance of a snow day in {}, {}. The forecast calls for {}, meaning there is a very good chance of a snow day. Would you like to ask again?".format(
-                    finalPrediction, city, state, summaryd) + message)
+            return question("There is a {} percent chance of a snow day in {}, {}. The forecast calls for {}, meaning there is a very good chance of a snow day. Would you like to ask again?".format(finalPrediction, city, state, summaryd) + message)
 
-        return question(
-            "There is a {} percent chance of a snow day in {}, {}. Would you like to ask again?".format(finalPrediction,
-                                                                                                        city,
-                                                                                                        state) + message)
+        return question("There is a {} percent chance of a snow day in {}, {}. Would you like to ask again?".format(finalPrediction,city,state) + message)
     except:
         return question("Sorry, I couldn't find any weather data for this zip code. Would you like to try again?")
-
-
 @ask.intent("HelpIntent")
 def help():
     msg = "This snow day prediction application uses the Dark Sky API to get weather data based on a location. The weather data collected is then compared to weather" \
@@ -206,16 +192,13 @@ def help():
           "Sci-kit Learn library in Python. To make a prediction, simply say your zip code when prompted, and a percentage will be returned. Would you like to try?"
     return question(msg)
 
-
 @ask.intent("YesIntent")
 def yes():
     return question("What is your zip code?")
 
-
 @ask.intent("NoIntent")
 def no():
     return statement("Goodbye!")
-
 
 if __name__ == "__main__":
     app.run(debug=True)
